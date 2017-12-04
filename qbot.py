7fd71d779f386d109cfe44be784dff2b1586ec09
alvivar <andresalvivar@gmail.com>
2017-12-04 13:38:44 -0600

    Design +1

--- qbot.py
+++ qbot.py
@@ -1,14 +1,24 @@
-import os
 import json
+import os
 import sys
 import time
+from enum import IntEnum
+
+DAYS = IntEnum(
+    "DAYS",
+    "Monday Tuesday Wednesday Thursday Friday Saturday Sunday",
+    start_at=0)
 
 
-def create_schedule(name, *, monday=False, tuesday=False, wednesday=False):
+def set_schedule(name, days_list, times_list):
     pass
 
 
 def queue_tweet(schedule_name, message=None, image=None):
+    pass
+
+
+def run():
     pass
 
 
@@ -38,5 +48,3 @@ if __name__ == "__main__":
         }
         with open(TOKENS_FILE, "w") as f:
             json.dump(TOKENS, f)
-
-    print(time.time())
