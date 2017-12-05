6b689b4f6138728162691ccfc1c2cb09bac3dbe5
alvivar <andresalvivar@gmail.com>
2017-12-05 12:56:15 -0600

    Main functions moved from the database to the main script

--- qbot.py
+++ qbot.py
@@ -4,21 +4,79 @@ import json
 import os
 import sys
 import time
-from enum import IntEnum
 
-# Database SQLite + SQLAlchemy
+# Database SQLAlchemy + SQLite
 import qdb
+
 ENGINE = qdb.init_database()
 SESSION = qdb.sessionmaker(bind=ENGINE)
 DB = SESSION()
 
 
-def set_schedule(name, *, days=[], hours=[]):
-    qdb.update_schedule(DB, name, days, hours)
+def update_schedule(name, days, hours):
+    """ Create or update and return a schedule, assuming days as a list of
+    numbers enumerating the days of the week (0-6) and hours a list of tuples of
+    hours and minutes. """
+
+    # Get
+    schedule = DB.query(qdb.Schedule).filter(qdb.Schedule.name == name).first()
+
+    # Or create
+    if schedule is None:
+        schedule = qdb.Schedule()
+        schedule.name = name
+        DB.add(schedule)
+        DB.flush()
+
+    # Days of the week
+    schedule.monday = True if 0 in days else False
+    schedule.tuesday = True if 1 in days else False
+    schedule.wednesday = True if 2 in days else False
+    schedule.thursday = True if 3 in days else False
+    schedule.friday = True if 4 in days else False
+    schedule.saturday = True if 5 in days else False
+    schedule.sunday = True if 6 in days else False
+
+    # New hours
+    DB.query(qdb.Time).filter(qdb.Time.schedule_id == schedule.id).delete()
+    for h_m in hours:
+        hour = qdb.Time()
+        hour.hour = h_m[0]
+        hour.minute = h_m[1]
+        hour.schedule_id = schedule.id
+        DB.add(hour)
+
+    # Save
+    DB.commit()
+
+    return schedule
 
 
-def queue_post(schedule_name, *, message=None, image=None):
-    pass
+def create_post(schedule_name, text, image_url):
+    """ Create a post, return it. If the schedule doesn't exist, create it. """
+
+    # Get
+    schedule = DB.query(
+        qdb.Schedule).filter(qdb.Schedule.name == schedule_name).first()
+
+    # Or create
+    if schedule is None:
+        schedule = qdb.Schedule()
+        schedule.name = schedule_name
+
+        DB.add(schedule)
+        DB.flush()
+
+    # New post
+    post = qdb.Post()
+    post.text = text
+    post.image_url = image_url
+    post.schedule_id = schedule.id
+
+    DB.add(post)
+    DB.commit()
+
+    return post
 
 
 def process_queue():
@@ -28,7 +86,7 @@ def process_queue():
 if __name__ == "__main__":
 
     DELTA = time.time()
-    print("twqbot\n")
+    print("QBot\n")
 
     # Frozen / not frozen, cxfreeze compatibility
     DIR = os.path.normpath(
--- qdb.py
+++ qdb.py
@@ -8,10 +8,11 @@ from sqlalchemy import (Boolean, Column, ForeignKey, Integer, String,
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 
-Base = declarative_base()
+BASE = declarative_base()
 
 
-class Schedule(Base):
+class Schedule(BASE):
+    """ Holds the schedule of publication, days and hours. """
     __tablename__ = "schedule"
     id = Column(Integer, primary_key=True)
     times = relationship("Time")
@@ -25,7 +26,8 @@ class Schedule(Base):
     sunday = Column(Boolean, default=False)
 
 
-class Time(Base):
+class Time(BASE):
+    """ Hours of publication in a Schedule. """
     __tablename__ = "time"
     id = Column(Integer, primary_key=True)
     schedule_id = Column(Integer, ForeignKey("schedule.id"))
@@ -33,7 +35,8 @@ class Time(Base):
     minute = Column(Integer)
 
 
-class Post(Base):
+class Post(BASE):
+    """ Posts to be published on Schedule time. """
     __tablename__ = "post"
     id = Column(Integer, primary_key=True)
     schedule_id = Column(Integer, ForeignKey("schedule.id"))
@@ -45,92 +48,24 @@ class Post(Base):
 
 def init_database():
     """ Create the database, return the engine. """
+
+    # The main dir should be the script dir
+    currdir = os.path.normpath(
+        os.path.dirname(
+            sys.executable if getattr(sys, 'frozen', False) else
+            __file__))  # Frozen / not frozen, cxfreeze compatibility
+    os.chdir(currdir)
+
     engine = create_engine("sqlite:///data.db")
-    Base.metadata.bind = engine
-    Base.metadata.create_all()
+    BASE.metadata.bind = engine
+    BASE.metadata.create_all()
+
     return engine
-
-
-def update_schedule(db, name, days, hours):
-    """ Create or update a schedule, assuming days as a list of numbers
-    enumerating the days of the week (0-6) and hours a list of tuples of hours
-    and minutes. """
-
-    # Get
-    schedule = db.query(Schedule).filter(Schedule.name == name).first()
-
-    # Or create
-    if schedule is None:
-        schedule = Schedule()
-        schedule.name = name
-        db.add(schedule)
-        db.flush()
-
-    # Days of the week
-    schedule.monday = True if 0 in days else False
-    schedule.tuesday = True if 1 in days else False
-    schedule.wednesday = True if 2 in days else False
-    schedule.thursday = True if 3 in days else False
-    schedule.friday = True if 4 in days else False
-    schedule.saturday = True if 5 in days else False
-    schedule.sunday = True if 6 in days else False
-
-    # New hours
-    db.query(Time).filter(Time.schedule_id == schedule.id).delete()
-    for hm in hours:
-        hour = Time()
-        hour.hour = hm[0]
-        hour.minute = hm[1]
-        hour.schedule_id = schedule.id
-        db.add(hour)
-
-    # Save db
-    db.add(schedule)
-    db.commit()
-
-
-def create_post(db, schedule_name, text, image_url):
-
-    # Get
-    schedule = db.query(Schedule).filter(
-        Schedule.name == schedule_name).first()
-
-    # Or create
-    if schedule is None:
-        schedule = Schedule()
-        schedule.name = schedule_name
-
-        db.add(schedule)
-        db.flush()
-
-    # New post
-    post = Post()
-    post.text = text
-    post.image_url = image_url
-    post.schedule_id = schedule.id
-
-    db.add(post)
-    db.commit()
 
 
 if __name__ == "__main__":
 
-    # Frozen / not frozen, cxfreeze compatibility
-    DIR = os.path.normpath(
-        os.path.dirname(
-            sys.executable if getattr(sys, 'frozen', False) else __file__))
-    os.chdir(DIR)
-
-    # Test
-
+    # Connection
     ENGINE = init_database()
     SESSION = sessionmaker(bind=ENGINE)
     DB = SESSION()
-
-    update_schedule(DB, "all days at 7", [0, 1, 2, 3, 4, 5, 6], [(7, 00),
-                                                                 (19, 0)])
-    update_schedule(DB, "weekend every 3 hours", [5, 6],
-                    [(10, 00), (13, 00), (16, 00), (19, 00), (22, 00)])
-
-    create_post(DB, "wthell", "is this even real?",
-                r"C:\Users\matnesis\Documents\Overwatch\ScreenShots\Overwatch")
