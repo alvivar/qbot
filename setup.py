4774df6027e90d888a4c5b07cdf9df8180a5999c
alvivar <andresalvivar@gmail.com>
2017-12-18 14:21:14 -0600

    cx_Freeze setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+""" Create an .exe with cx_Freeze, just call 'python setup.py build'. You need
+to change the TCL, TK environment variables below to your own. """
+
+from cx_Freeze import Executable, setup
+
+OPTIONS = {'build_exe': {'includes': ['sqlalchemy.sql.default_comparator']}}
+
+EXECUTABLES = [Executable('qbot.py', targetName='qbot.exe')]
+
+setup(
+    name='QBot',
+    version='0.1',
+    description=
+    "Bot that tweets on schedules, using json files as configuration",
+    executables=EXECUTABLES,
+    options=OPTIONS)
